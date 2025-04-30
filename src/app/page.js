fc3db7e7a1dd616a020bdbaaeef8f661d389885b
BR117 <birahoma@outlook.com>
2025-04-30 20:25:30 +0100

    deploy update

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,17 @@ import myImage from '../assets/space.png'
 
 export default function HomePage() {
   return (
-    <section className="home">
-      <div className="home-left">
-        <Image src={myImage} alt="Portfolio Image" width={400} height={400} />
-      </div>
-      <div className="home-right">
-        <h1>Hey there!</h1>
-        <p>Feel free to explore!</p>
-      </div>
-    </section>
+    <section className="main-content">
+  <div className="home">
+    <div className="home-left">
+      <Image src={myImage} alt="Portfolio Image" width={400} height={400} />
+    </div>
+    <div className="home-right">
+      <h1>Hey there!</h1>
+      <p>Feel free to explore!</p>
+    </div>
+  </div>
+</section>
+
   )
 }
